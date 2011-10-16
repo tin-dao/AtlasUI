09720c6bf2f3e84b83ec138e26faadf5ac2dfdea
Joshua Strobl <truthfromlies@gmail.com>
2011-10-16 03:07:48 -0700

    Added Twitter Profile module

--- Framework/social/twitter.php
+++ Framework/social/twitter.php
@@ -34,4 +34,108 @@
 
 	}
 
+	function twitter_profile($profile, $width, $height, $shell_bg, $shell_text, $tweets_bg, $tweets_text, $tweets_links, $show_hashtags, $show_timestamps, $show_avatars)
+	{
+		print "<script src=\"http://widgets.twimg.com/j/2/widget.js\"></script>";
+		print "<script>";
+		print "
+			new TWTR.Widget({
+			  version: 2,
+			  type: 'profile',
+			  rpp: 4,
+			  interval: 30000,";
+		if ($width == "")
+		{
+			print "width: 250,";
+		}
+		else{
+			print "width: $width,";
+		}
+		
+		if ($height == "")
+		{
+			print "height: 300,";
+		}
+		else{
+			print "height: $height,";
+		}
+		print "
+			  theme: {
+				shell: {";
+		if ($shell_bg == "")
+		{
+			print "background: '#ffffff',";
+		}
+		else{
+			print "background: '#$shell_bg',";
+		}
+		if ($shell_text == "")
+		{
+			print "color: '#0099ff'";
+		}
+		else{
+			print "color: '#$shell_text'";
+		}
+		print "},
+				tweets: {";
+		
+		if ($tweets_bg == "")
+		{
+			print "background: '#ffffff',";
+		}
+		else{
+			print "background: '#$tweets_bg',";
+		}
+		
+		if ($tweets_text == "")
+		{
+			print "color: '#333333',";
+		}
+		else{
+			print "color: '#$tweets_text',";
+		}
+		
+		if ($tweets_links == "")
+		{
+			print "links: '#0099ff'";
+		}
+		else{
+			print "links: '#$tweets_links'";
+		}
+		print "
+				}
+			  },
+			  features: {
+				scrollbar: false,
+				loop: false,
+				live: false,";
+		if ($show_hashtags == ("" || "yes" || "true"))
+		{
+			print "hashtags: true,";
+		}
+		else{
+			print "hashtags: false,";
+		}
+		if ($show_timestamps == ("" || "yes" || "true"))
+		{
+			print "timestamp: true,";
+		}
+		else{
+			print "timestamp: false,";
+		}
+		
+		if ($show_avatars == ("" || "yes" || "true"))
+		{
+			print "avatars: true,";
+		}
+		else{
+			print "avatars: false,";
+		}
+		
+		print "
+				behavior: 'all'
+			  }
+			}).render().setUser('$profile').start();";
+		print "</script>";
+	}
 ?>
