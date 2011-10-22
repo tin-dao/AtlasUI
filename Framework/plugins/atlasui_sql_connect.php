c2c16a66fe32ae6d1eff70b16d0de72cd2149f52
Joshua Strobl <truthfromlies@gmail.com>
2011-10-22 03:24:21 -0700

    Fixed AtlasUI SQL Connect module

--- Framework/plugins/atlasui_sql_connect.php
+++ Framework/plugins/atlasui_sql_connect.php
@@ -2,13 +2,13 @@
 	
 	function atlasui_sql_connect($location, $database, $user, $password)
 	{
-		$go = mysql_connect('$location', '$user', '$password');
+		$go = mysql_connect($location, $user, $password);
 		if (!$go)
 		{
 			die('Could not connect: ' . mysql_error());
 		}
 		else{
-			mysql_select_db('$database', $go);
+			mysql_select_db($database, $go);
 		}
 	}
 	
