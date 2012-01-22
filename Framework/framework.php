d8660e3df0213ca7db766b9d8f4bc29283aab87c
Joshua Strobl <truthfromlies@gmail.com>
2012-01-22 13:46:56 -0800

    Added error_reporting trigger to eliminate header confliction errors from showing.

--- Framework/framework.php
+++ Framework/framework.php
@@ -24,58 +24,61 @@
 			copy of AtlasUI Web Framework, go to http://github.com/JoshStrobl/AtlasUI for more information -->
 	
 		<?php
-		  /* Import Audio and Video Framework */
-
-			include("media/atlasui_cc_buttons.php");
-			include("media/audio.php");
-			include("media/video.php");
 			
-		  /* Import AtlasUI Drawing and Graphics Framework */
+			error_reporting(E_ERROR | E_WARNING | E_PARSE);
 			
-			include("drawing/draw_heart.php");
+			/* Import Audio and Video Framework */
 
-		  /* Import AtlasUI Main Modules */
-
-			include("main/contactform.php");
-			include("main/menubar.php");
-			include("main/module.php");
-			include("main/search.php");
-			include("main/topbar.php");
-
-		  /* Import AtlasUI Browser Interaction Modules */
+				include("media/atlasui_cc_buttons.php");
+				include("media/audio.php");
+				include("media/video.php");
 			
-			include("browser/notification.php");
-			include("browser/redirect.php");
-			include("browser/ymp_install.php");
+			/* Import AtlasUI Drawing and Graphics Framework */
 			
-		  /* Import Math Framework / Library */
+				include("drawing/draw_heart.php");
+
+			/* Import AtlasUI Main Modules */
+
+				include("main/contactform.php");
+				include("main/menubar.php");
+				include("main/module.php");
+				include("main/search.php");
+				include("main/topbar.php");
+
+			/* Import AtlasUI Browser Interaction Modules */
+			
+				include("browser/notification.php");
+				include("browser/redirect.php");
+				include("browser/ymp_install.php");
+			
+			/* Import Math Framework / Library */
 		  
-			include("math/alibmath.php");
+				include("math/alibmath.php");
 
-		  /* Import Modular Communication Framework & Other Plugins */
+			/* Import Modular Communication Framework & Other Plugins */
 
-			include("plugins/atlasui_encrypt.php");
-			include("plugins/atlasui_ftp_login.php");
-			include("plugins/atlasui_ip_address.php");
-			include("plugins/atlasui_sql_connect.php");
-			include("plugins/atlasui_url_address.php");
-			include("plugins/opengraph.php");
-			include("plugins/google_maps_static.php");
-			include("plugins/paypal.php");
-			include("plugins/powered_by.php"); // Powered by AtlasUI Web Framework banner - integration is completely optional of course //
+				include("plugins/atlasui_encrypt.php");
+				include("plugins/atlasui_ftp_login.php");
+				include("plugins/atlasui_ip_address.php");
+				include("plugins/atlasui_sql_connect.php");
+				include("plugins/atlasui_url_address.php");
+				include("plugins/opengraph.php");
+				include("plugins/google_maps_static.php");
+				include("plugins/paypal.php");
+				include("plugins/powered_by.php"); // Powered by AtlasUI Web Framework banner - integration is completely optional of course //
 
-		  /* Import Social Network Framework */
+			/* Import Social Network Framework */
 
-			include("social/facebook.php");
-			include("social/flattr_button.php");
-			include("social/google_buzz.php");
-			include("social/google_plus_badge.php");
-			include("social/google_plus_one.php");
-			include("social/evovid.php");
-			include("social/reddit.php");
-			include("social/spotify.php");
-			include("social/twitter.php");
-			include("social/youtube.php");
+				include("social/facebook.php");
+				include("social/flattr_button.php");
+				include("social/google_buzz.php");
+				include("social/google_plus_badge.php");
+				include("social/google_plus_one.php");
+				include("social/evovid.php");
+				include("social/reddit.php");
+				include("social/spotify.php");
+				include("social/twitter.php");
+				include("social/youtube.php");
 
 		?>
 		
