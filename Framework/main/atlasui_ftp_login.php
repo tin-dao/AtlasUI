4ee21da9a458b5462a718d4fd2d28e7e3d8410b9
Joshua Strobl <truthfromlies@gmail.com>
2013-04-25 05:10:12 -0700

    Applied fix to FTP login where particular resource type checking caused PHP to provide obscure info.

--- Framework/main/atlasui_ftp_login.php
+++ Framework/main/atlasui_ftp_login.php
@@ -2,6 +2,7 @@
 
 	function atlasui_ftp_login($ftp_server, $ssl_or_not, $username, $password){
 		$clean_ftp_location = str_replace("ftp://", "ftp.", $ftp_server);
+
 		if ($ssl_or_not !== true){
 			$ftp_connection_passed = ftp_connect($clean_ftp_location) or die("atlasui_ftp_login failed to connect.");
 		}
@@ -9,12 +10,12 @@
 			$ftp_connection_passed = ftp_ssl_connect($clean_ftp_location) or die("atlasui_ftp_login failed to connect.");
 		}
 		
-		if (gettype($ftp_connection_passed) !== "string"){
-			if (ftp_login($ftp_connection_passed, $username, $password) == false){
-				return "atlasui_ftp_login has invalid credentials. Trying to connect to $ftp_server (clean: $clean_ftp_location) with $username and $password";
-			}
+		if (ftp_login($ftp_connection_passed, $username, $password) == false){
+			die("atlasui_ftp_login has invalid credentials. Trying to connect to $ftp_server (clean: $clean_ftp_location) with $username and $password");
 		}
-		return $ftp_connection_passed;
+		else{
+			return $ftp_connection_passed;
+		}
 		
 		/* When using atlasui_ftp_login, remember to assign a variable equal to the function, as the function will
 		 * return the ftp_connect should it be correct. This will then allow you to access all ftp features just
