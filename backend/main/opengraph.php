cdd9adef49ee94d69eaa4b1146986bfdcbb0e825
Joshua Strobl <truthfromlies@gmail.com>
2014-04-12 22:18:51 +0300

    Fixes and improvements to OpenGraph function. Updated version to 0.40.1 and rebuilt the zip files.

--- backend/main/opengraph.php
+++ backend/main/opengraph.php
@@ -1,6 +1,6 @@
 <?php
 
-	function atlasui_opengraph($title, $description, $site_name, $type, $url = null, $image = null){
+	function atlasui_opengraph($title, $description, $site_name, $type, $url = "dynamic", $image = null, array $optionalMetaData = null){
 		print "<meta property=\"og:title\" content=\"$title\" />";
 		print "<meta property=\"og:description\" content=\"$description\" />";
 		print "<meta property=\"og:site_name\" content=\"$site_name\" />";
@@ -18,7 +18,7 @@
 			print "<meta property=\"og:country-name\" content=\"$countryName\" />";			
 		}
 		
-		if ($url == null){ // If the URL is NOT defined, generate it. Else, use defined one.
+		if ($url == "dynamic"){ // If the URL is NOT defined, generate it. Else, use defined one.
 			$url = atlasui_url_address();
 		}
 
@@ -26,6 +26,12 @@
 
 		if ($image !== null){ // If an OpenGraph image is defined, use it.
 			print "<meta property=\"og:image\" content=\"$image\" />";
+		}
+
+		if ($optionalMetaData !== null){ // If there is other metadata defined
+			foreach ($optionalMetaData as $optionalMetaDataKey => $optionalMetaDataValue){ // For every key/val metadata defined
+				print "<meta property=\"$optionalMetaDataKey\" content=\"$optionalMetaDataValue\" />";
+			}
 		}
 	}
 
