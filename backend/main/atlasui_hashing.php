a05acb8f2d2b06fae17a5c57c07880e0a40cec61
Josh Strobl <JoshStrobl@users.noreply.github.com>
2014-05-09 05:20:27 +0300

    Cleaned up atlasui_hashing
    Removed two duplicate declarations of the full salt string, cleaned up code and did some needed documentation as well as vernacular changes in variable names to avoid any confusion between encryption and hashing.

--- backend/main/atlasui_hashing.php
+++ backend/main/atlasui_hashing.php
@@ -1,11 +1,13 @@
 <?php
 
-	function atlasui_hashing($string, $rounds = 50000, $hashString = "atlasui"){
-		$cleanLength = strlen('$6$rounds=' . $rounds . "$" . $hashString . "$");
-		$initial_encryption = sha1(md5($string));
-		$hash = str_replace(array("/", '\"', '"'), "", crypt($initial_encryption, '$6$rounds=' . $rounds . '$' . $hashString . '$'));
-		$encrypted_string = substr($hash, $cleanLength);
-		return $encrypted_string;
+	function atlasui_hashing($string, $rounds = 50000, $specifiedSalt = "atlasui"){
+		$cryptPrefix = '$6$rounds=' . $rounds . "$" . $specifiedSalt . "$"; // Create a salt string of sha512, rounds specified, and the specifiedSalt provided
+		$hashRemovalLength = strlen($cryptPrefix); // Determine how much of the string we'll need to remove, otherwise the hash will be prefixed with the type
+		
+		$initialHashing = sha1(md5($string)); // Do an initial hashing of the string by sha1'ing the md5 of the string
+		$fullLengthHashing = str_replace(array("/", '\"', '"'), "", crypt($initialHashing, $cryptPrefix)); // Run the string through crypt, remove forward and back slashes as well as double quote
+		$hashedString = substr($fullLengthHashing, $hashRemovalLength); // Remove the string in front of the hash that exposes the type of crypt used
+		return $hashedString;
 	}
 	
 ?>
